489fb3511f246912b03936bc40d3f24698b6fe23
tabramova <abramova0555@mail.ru>
2025-11-03 01:47:28 +0300

    added pytest marks

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -1,13 +1,17 @@
+import pytest
+@pytest.mark.smoke
 def test_check_page_section_title_is(cart_page):
     cart_page.open_page()
     cart_page.check_page_section_title_is('Order overview')
 
 
+@pytest.mark.regression
 def test_check_message_in_the_section(cart_page):
     cart_page.open_page()
     cart_page.check_message_in_the_section('Your cart is empty!')
 
 
+@pytest.mark.extended
 def test_check_transition_to_logo(cart_page):
     cart_page.open_page()
     cart_page.check_transition_to_logo()
